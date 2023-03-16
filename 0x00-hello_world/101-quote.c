e15718bd7f1cc46acc0bf7580e956225c5f67e33
m-bachar <m.aminebachar@gmail.com>
2023-03-16 16:32:22 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,28 @@
 #include <unistd.h>
 
 /**
+ * myPutstr - A function that prints char *str char by char in the std output
+ * @str: The argument passed to the function to be printed
+ */
+void	myPutstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+/**
  * main - Entry point
- * Return: Always 0 (Success)
+ *
+ * Return: 1 Uppon Success
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	myPutstr("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
