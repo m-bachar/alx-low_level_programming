e5e51ace7b20b91087e7d4fdd3c5725e8bc452b6
m-bachar <m.aminebachar@gmail.com>
2023-03-23 17:35:10 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+/**
+ * _isupper - Checkes for uppercase character
+ * @c: Character to be checked
+ * Return: returns 1 if the character is uppercase
+ * 	return 0 if the character is not an uppercase
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
