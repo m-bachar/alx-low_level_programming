d5277f5c362eb07a88ff37c5558e5e7602d08262
Mohamed Amine Bachar <m.aminebachar@gmail.com>
2023-04-15 12:24:00 +0000

    added

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -26,11 +26,11 @@ int	main(int argc, char **argv)
 		coins++;
 		if ((cents - 25) >= 0)
 			cents -= 25;
-		else if ((cents - 10) >= 0)
+		if ((cents - 10) >= 0)
 			cents -= 10;
-		else if ((cents - 5) >= 0)
+		if ((cents - 5) >= 0)
 			cents -= 5;
-		else if ((cents - 2) >= 0)
+		if ((cents - 2) >= 0)
 			cents -= 2;
 		cents--;
 	}
