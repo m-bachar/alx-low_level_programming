dabeabb02a6e18cbe0b97f9f0964672825e4f093
Mohamed Amine Bachar <m.aminebachar@gmail.com>
2023-04-15 12:47:11 +0000

    added

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,13 @@ char	*_strchr(char *s, char c)
 {
 	int	i;
 
-	i = 0;
-	while (s[i])
+	while (*(s + i))
 	{
-		if (s[i] == c)
+		if (*(s + i) == c)
 			return (s + i);
 		i++;
 	}
+	if (*(s + i) == c)
+		return (s + i);
 	return (0);
 }
