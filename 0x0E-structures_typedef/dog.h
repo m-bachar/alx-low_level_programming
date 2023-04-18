6dbbfec2b60c72308b0a0bf2678b2e9655fef16e
Mohamed Amine Bachar <m.aminebachar@gmail.com>
2023-04-18 01:01:13 +0000

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+
+/**
+  * dog - struct named dog
+  * @name: element of struct dog type char *
+  * @age: element of struct dog type float
+  * @owner: element of struct dog type char *
+  */
+
 struct	dog
 {
 	char	*name;
