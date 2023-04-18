41c9c4f3f23dbb6cd52d7b0f1fb6c70e21d941a5
Mohamed Amine Bachar <m.aminebachar@gmail.com>
2023-04-18 02:33:56 +0000

    A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,35 +5,23 @@
   * @d: struct dog
   */
 
-// void	print_dog(struct dog *d)
-// {
-// 	if (d)
-// 	{
-// 		if (d->name == NULL)
-// 			printf("Name : (nil)\n");
-// 		else
-// 			printf("Name : %s\n", d->name);
-// 		if (!d->age)
-// 			printf("Age : (nil)\n");
-// 		else
-// 			printf("Age : %f\n", d->age);
-// 		if (d->owner == NULL)
-// 			printf("Owner : (nil)\n");
-// 		else
-// 			printf("Owner : %s\n", d->owner);
-// 	}
-// 	else
-// 		return;
-// }
-
 void	print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d)
 	{
-		printf("Name: ");
-		d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
-		printf("Age: %f\n", d->age);
-		printf("Owner: ");
-		d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
+		if (d->name == NULL)
+			printf("Name : (nil)\n");
+		else
+			printf("Name : %s\n", d->name);
+		if (!d->age)
+			printf("Age : (nil)\n");
+		else
+			printf("Age : %f\n", d->age);
+		if (d->owner == NULL)
+			printf("Owner : (nil)\n");
+		else
+			printf("Owner : %s\n", d->owner);
 	}
+	else
+		return;
 }
