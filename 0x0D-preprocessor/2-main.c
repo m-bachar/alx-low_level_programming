2cd93362d6ac0b2e2d0f2100a551bc519237cb0c
Mohamed Amine Bachar <m.aminebachar@gmail.com>
2023-04-18 13:46:52 +0000

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+/**
+  * main -  A program that prints the name of 
+  *     the file it was compiled from
+  * Return: Returns 0 uppon Success
+  */
+
+int	main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
