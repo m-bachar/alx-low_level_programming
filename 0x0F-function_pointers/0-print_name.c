de7c938cc20dffd0bfc9f91784334d611534645c
Mohamed Amine Bachar <m.aminebachar@gmail.com>
2023-04-20 10:48:23 +0000

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,9 +2,8 @@
 
 /**
   * print_name - A function that prints a name.
-  * 
-  * @name: Name to be printed 
-  * @f: Pointer to function. 
+  * @name: Name to be printed
+  * @f: Pointer to function.
   */
 
 void	print_name(char *name, void (*f)(char *))
