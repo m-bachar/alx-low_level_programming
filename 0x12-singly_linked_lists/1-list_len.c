71253c25e3163f647bd84c3d637e8202c2a4524a
Mohamed Amine Bachar <m.aminebachar@gmail.com>
2023-04-27 21:34:54 +0100

    List Length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - A function that returns the length of a linked list.
+ *
+ * @h: list_t.
+ * Return: Returns the length of a linked list.
+ */
+
+size_t	list_len(const list_t *h)
+{
+	size_t	i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
