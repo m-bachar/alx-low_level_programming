b2bac9dd7076814babd638862f3ff0e3bda4a5ce
Mohamed Amine Bachar <m.aminebachar@gmail.com>
2023-04-28 22:42:45 +0100

    Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,36 +10,10 @@ void	free_list(list_t *head)
 {
 	list_t	*temp;
 
-	while (head != NULL)
-	{
-		temp = head;
-		free(temp);
-		head = head->next;
-	}
-}
-
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    list_t *head;
-
-    head = NULL;
-    add_node_end(&head, "Bob");
-    add_node_end(&head, "&");
-    add_node_end(&head, "Kris");
-    add_node_end(&head, "love");
-    add_node_end(&head, "asm");
-    print_list(head);
-    free_list(head);
-    head = NULL;
-    return (0);
+	if (head == NULL)
+		return ;
+	free_list(head->next);
+	if (head->str)
+		free(head->str);
+	free(head);
 }
