2f51d81512862827c1a2930c712402a125ca0e38
Mohamed Amine Bachar <m.aminebachar@gmail.com>
2023-04-28 22:11:11 +0100

    Free kust

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * free_list - A function that free a list_t.
+ *
+ * @head: Head of the list.
+ */
+
+void	free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		free(head);
+		head = head->next;
+	}
+}
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    list_t *head;
+
+    head = NULL;
+    add_node_end(&head, "Bob");
+    add_node_end(&head, "&");
+    add_node_end(&head, "Kris");
+    add_node_end(&head, "love");
+    add_node_end(&head, "asm");
+    print_list(head);
+    free_list(head);
+    head = NULL;
+    return (0);
+}
